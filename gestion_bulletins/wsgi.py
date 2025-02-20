6223882719afc2a08265c4513ad187b57eb82fdd
Ibrahima WADE <wibrahima@gmail.com>
2025-02-20 22:32:32 +0000

    modification du fichier wsgi

--- gestion_bulletins/wsgi.py
+++ gestion_bulletins/wsgi.py
@@ -10,7 +10,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_bulletins.settings')
 
 application = get_wsgi_application()
+
+# Exécuter les migrations automatiquement au démarrage
+try:
+    call_command("migrate")
+except Exception as e:
+    print(f"Erreur lors de la migration : {e}")
