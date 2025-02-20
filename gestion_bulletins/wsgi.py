2ad65e0c8333730e7cd89c2c6006f7ccfa5083fc
Ibrahima WADE <wibrahima@gmail.com>
2025-02-20 22:38:38 +0000

    Ajout du superuser dans wsgi

--- gestion_bulletins/wsgi.py
+++ gestion_bulletins/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from django.core.management import call_command
+from django.contrib.auth import get_user_model
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_bulletins.settings')
 
@@ -21,3 +22,9 @@ try:
     call_command("migrate")
 except Exception as e:
     print(f"Erreur lors de la migration : {e}")
+
+# Création automatique du superutilisateur (exécute une seule fois)
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "wibrahima@gmail.com", "P@lfren1er")
+    print("Superutilisateur créé avec succès !")    
